ad15ca2d0f4b3ead4c9999b343cf09daa385cac0
hyper <honkhonk4456@gmail.com>
2025-02-01 16:40:32 -0500

    Changed villager checks (again)

--- src/main/java/dev/heypr/tradeflux/commands/DAVCommand.java
+++ src/main/java/dev/heypr/tradeflux/commands/DAVCommand.java
@@ -21,14 +21,12 @@ public class DAVCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         plugin.getScanner().cancelTaskTimers();
-        int simulationDistance = Bukkit.getServer().getSimulationDistance();
-        int scanRadius = simulationDistance * 8;
         BukkitRunnable task = new BukkitRunnable() {
             @Override
             public void run() {
                 for (Player player : Bukkit.getServer().getOnlinePlayers()) {
                     for (Villager villager : player.getWorld().getEntitiesByClass(Villager.class)) {
-                        if (villager.getLocation().distance(player.getLocation()) <= scanRadius && villager.getProfession() != Villager.Profession.NONE && !villager.isAware()) {
+                        if (villager.isAware()) {
                             villager.setAware(false);
                         }
                     }
